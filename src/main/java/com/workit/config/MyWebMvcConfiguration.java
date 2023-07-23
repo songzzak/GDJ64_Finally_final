fcc0fb81e771333a0a010b5eef3fabaf83d64544
yoonjin <yoonzin@DESKTOP-VUCI2ME>
2023-07-24 02:11:03 +0900

    프로젝트세팅

---
+++ src/main/java/com/workit/config/MyWebMvcConfiguration.java
@@ -0,0 +1,18 @@
+package com.workit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@Configuration
+@EnableWebSocket
+public class MyWebMvcConfiguration implements WebMvcConfigurer{
+
+	//view설정
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
+
+}
